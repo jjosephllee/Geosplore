d5a29d3ec41cd8e0e24a0b17637795f9d7c3c81a
jjosephllee <jjosephllee05@gmail.com>
2022-10-22 11:29:06 -0400

    Set up flask

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World"
+def index():    
+    return render_template("index.html", name="john")
 
 
-if __name__ == "__main__";
+if __name__ == "__main__":
     app.run(debug=True)
 
